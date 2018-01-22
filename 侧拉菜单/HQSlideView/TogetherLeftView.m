ee48491c11c2f10495f9c3d4d7a059599b9ed97d
何青 <qhe@iyouxin.com>
2018-01-22 17:13:32 +0800

    first commit

---
+++ 侧拉菜单/HQSlideView/TogetherLeftView.m
@@ -0,0 +1,21 @@
+//
+//  TogetherLeftView.m
+//  侧拉菜单
+//
+//  Created by 何青 on 2018/1/22.
+//  Copyright © 2018年 何青. All rights reserved.
+//
+
+#import "TogetherLeftView.h"
+
+@implementation TogetherLeftView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ 侧拉菜单/HQSlideView/TogetherRightView.h
@@ -0,0 +1,13 @@
+//
+//  TogetherRightView.h
+//  侧拉菜单
+//
+//  Created by 何青 on 2018/1/22.
+//  Copyright © 2018年 何青. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TogetherRightView : UIView
+
+@end
