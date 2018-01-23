a8fed7359d44780fa46396856ecc41f9bd1d29f7
何青 <qhe@iyouxin.com>
2018-01-23 09:33:58 +0800

    stumansys

---
+++ 侧拉菜单/HQHttpTool/HQHttpTool.h
@@ -0,0 +1,13 @@
+//
+//  HQHttpTool.h
+//  侧拉菜单
+//
+//  Created by 何青 on 2018/1/22.
+//  Copyright © 2018年 何青. All rights reserved.
+//
+
+#import "AFHTTPSessionManager.h"
+
+@interface HQHttpTool : AFHTTPSessionManager
+
+@end
